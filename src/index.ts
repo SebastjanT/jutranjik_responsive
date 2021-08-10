79976362f691a48439281a2f5b8b6ae385cf40f5
SebastjanT <sebastjan.tkavc@gmail.com>
2021-08-10 18:44:40 +0000

    Configured the database closes #4

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ require('dotenv').config();
 
 //  Create the winston logger
 require('./log/logger.ts');
+
+const { createStore } = require('./models/db.ts');
+
+//  Create the sequelize store
+createStore();
